0f5b2372f9f0a772ff182236805ece3e733e1d38
KembrijHome <AChere8@yandex.ru>
2020-04-29 01:21:25 +0300

    Added two tasks!

---
+++ week_1/пигвины.py
@@ -0,0 +1,9 @@
+num = int(input())
+
+str1 = "   _~_    "
+str2 = "  (o o)   "
+str3 = " /  V  \\  "
+str4 = "/(  _  )\\ "
+str5 = "  ^^ ^^   "
+
+print(str1 * num, str2 * num, str3 * num, str4 * num, str5 * num, sep="\n")
